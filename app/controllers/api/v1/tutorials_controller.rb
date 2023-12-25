b85af9d588cda334747f7f811cb8e3a35f1c66fb
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-25 08:30:27 -0300

    21° commit - tdd controller: Consegue listar um tutorial especifico e retornar status 200? GREEN

--- app/controllers/api/v1/tutorials_controller.rb
+++ app/controllers/api/v1/tutorials_controller.rb
@@ -1,10 +1,14 @@
 class Api::V1::TutorialsController < ApplicationController
 
-  #before_action :set_tutorial, only: %i[] # show update destroy
+  before_action :set_tutorial, only: %i[show] # show update destroy
 
   def index
     @tutorials = Tutorial.all 
     render json: @tutorials
+  end
+
+  def show
+    render json: @tutorial
   end
 
 private

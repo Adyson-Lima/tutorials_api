856803af66c0aa23214743f6bc735e03be394ee8
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-25 08:55:57 -0300

    28° commit - seeds.rb instalado

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts 'gerando tutorials...'
+ 5.times do
+   Tutorial.create(subject: ['rails', 'mainframe', 'openbsd'].sample, author: 'eu')
+ end
+puts 'tutorials gerados!'

8f47bd697508332d0aceee7dca9e91e3b4b12436
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-25 08:44:31 -0300

    25° commit - tdd controller: Consegue atualizar um tutorial e retornar status 200? GREEN

--- app/controllers/api/v1/tutorials_controller.rb
+++ app/controllers/api/v1/tutorials_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::TutorialsController < ApplicationController
 
-  before_action :set_tutorial, only: %i[show] # show update destroy
+  before_action :set_tutorial, only: %i[show update] # show update destroy
 
   def index
     @tutorials = Tutorial.all 
@@ -15,6 +15,14 @@ class Api::V1::TutorialsController < ApplicationController
     @tutorial = Tutorial.new(tutorial_params)
     if @tutorial.save
       render json: @tutorial, status: :created, location: api_v1_tutorial_url(@tutorial)
+    else
+      render json: @tutorial.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @tutorial.update(tutorial_params)
+      render json: @tutorial
     else
       render json: @tutorial.errors, status: :unprocessable_entity
     end

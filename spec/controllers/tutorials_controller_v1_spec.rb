cddb6d98c347395dcc65dae564b1cd06f0260133
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-25 08:29:21 -0300

    20° commit - tdd controller: Consegue listar um tutorial especifico e retornar status 200? RED

--- spec/controllers/tutorials_controller_v1_spec.rb
+++ spec/controllers/tutorials_controller_v1_spec.rb
@@ -12,4 +12,12 @@ RSpec.describe Api::V1::TutorialsController, type: :controller do
     end
   end
 
+  describe 'GET /api/v1/tutorials/id' do
+    it 'Consegue listar um tutorial especifico e retornar status 200?' do
+      get :show, params: {id: @tutorial.id}
+      expect(response.body).to include_json(id: @tutorial.id)
+      expect(response).to have_http_status(200)
+    end
+  end
+
 end
